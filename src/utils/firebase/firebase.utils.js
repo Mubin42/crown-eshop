c6129abfb0b759449a21c1eeaccdff3d00db7a64
Mubin <kazimubin.46@gmail.com>
2022-06-18 01:09:26 +0600

    v7

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {
   getAuth, 
   signInWithRedirect, 
   signInWithPopup,
-  GoogleAuthProvider
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from 'firebase/auth';
 import {
   getFirestore,
@@ -25,18 +26,28 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
   prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const SignInWithGooglePopup = () => signInWithPopup(auth,provider);
+export const SignInWithGooglePopup = () => 
+  signInWithPopup(auth,googleProvider);
+export const signInWithGoogleRedirect = () => 
+  signInWithRedirect(auth,googleProvider);
 
 export const db = getFirestore();
 
 
-export const createUserDocumentFromAuth = async(userAuth) => {
+export const createUserDocumentFromAuth = async(
+  userAuth, 
+  additionalInformation = {}
+  ) => {
+  
+    if(!userAuth) return;
+  
   const userDocRef = doc(db, 'users', userAuth.uid);
   const userSnapshot = await getDoc(userDocRef);
   
@@ -45,7 +56,12 @@ export const createUserDocumentFromAuth = async(userAuth) => {
     const createdAt = new Date();
 
     try {
-      await setDoc(userDocRef, {displayName,email,createdAt});
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation
+      });
     } catch(error) {
       console.log('error creating the user', error.masage);
     }
@@ -53,3 +69,10 @@ export const createUserDocumentFromAuth = async(userAuth) => {
 
   return userDocRef;
 };
+
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if(!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
