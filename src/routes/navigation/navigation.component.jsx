32b464ef6bad743c4712738f87ead192a888b110
Mubin <kazimubin.46@gmail.com>
2022-06-20 18:35:06 +0600

    shopping cart

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,12 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+
 import { ReactComponent as CrownLogo} from '../../assets/crown.svg';
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import './navigation.styles.scss';
@@ -10,6 +14,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const {currentUser} = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   
   
@@ -35,8 +40,9 @@ const Navigation = () => {
               Sign In
             </Link>)
             }
-            
+            <CartIcon />
           </div>
+          {isCartOpen && <CartDropdown />}
         </div>
         <Outlet />
       </Fragment>
