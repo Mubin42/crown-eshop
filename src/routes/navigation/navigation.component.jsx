3ad22a5312395271cf735767b3e565f043f5d821
Mubin <kazimubin.46@gmail.com>
2022-07-18 21:42:45 +0600

    almost final

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -9,7 +9,12 @@ import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import './navigation.styles.scss';
+import {
+  NavigationContainer, 
+  NavLinks, 
+  NavLink, 
+  LogoContainer
+} from './navigation.styles';
 
 
 const Navigation = () => {
@@ -20,30 +25,30 @@ const Navigation = () => {
   
     return(
       <Fragment>
-        <div className="navigation">
-          <Link className="logo-container" to={'/'}>
+        <NavigationContainer>
+          <LogoContainer to='/'>
             <CrownLogo className="logo"/>
-          </Link>
-          <div className="nav-links-container">
-            <Link className="nav-link" to={'/shop'}>
+          </LogoContainer>
+          <NavLinks>
+            <NavLink to={'/shop'}>
               Shop
-            </Link>
+            </NavLink>
             {
               currentUser ? (
-                <span 
-                  className="nav-link" 
+                <NavLink 
+                  as='span'
                   onClick={signOutUser}>
                     Sign Out
-                </span>)
+                </NavLink>)
 
-              : (<Link className="nav-link" to={'/auth'}>
+              : (<NavLink to={'/auth'}>
               Sign In
-            </Link>)
+            </NavLink>)
             }
             <CartIcon />
-          </div>
+          </NavLinks>
           {isCartOpen && <CartDropdown />}
-        </div>
+        </NavigationContainer>
         <Outlet />
       </Fragment>
     );
